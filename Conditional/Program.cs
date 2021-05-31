26f00d793baea20d9f29ffb01bd022b8e827ae1a
hilalozturkk <ozhilal222@gmail.com>
2021-06-01 02:06:53 +0300

    conditional

---
+++ Conditional/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Conditional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour; //şuanki saat
+
+            if(time >=6 && time<11){ 
+                Console.WriteLine("Günaydın");   
+            }
+            else if (time <= 18)
+            {
+                Console.WriteLine("İyi günler");   
+            }
+            else{
+                Console.WriteLine("İyi geceler");   
+            }
+
+            string sonuc = time <= 18 ? "iyi günler" : "iyi geceler";
+            Console.WriteLine(sonuc);
+
+            sonuc = time >= 6 ? "Günaydın" : time <=18 ? "İyi günler" : "İyi geceler";
+            Console.WriteLine(sonuc);
+        }
+    }
+}

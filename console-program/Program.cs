4ac7b0beae77ff8bf85bf45674abf602afb0b3a2
hilalozturkk <ozhilal222@gmail.com>
2021-05-31 14:54:01 +0300

    console commit

---
+++ console-program/Program.cs
@@ -0,0 +1,18 @@
+﻿﻿using System;
+
+namespace console_program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Adınızı giriniz : ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Soyadınızı giriniz :");
+            string surname = Console.ReadLine();
+
+            Console.WriteLine("Hoşgeldin "+name+" "+surname);
+            
+        }
+    }
+}

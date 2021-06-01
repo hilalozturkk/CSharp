01bbf8ec46ffa17059951dbc3fb8d38e485a12f8
hilalozturkk <ozhilal222@gmail.com>
2021-06-01 14:25:04 +0300

    cSharp

---
+++ error_handling/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace error_handling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           /*  Bir hata ile karşılaştığımızda onu hızlıca bulabilmek için Debug kullanırız
+           *   breakpoint ile kodu derlerken istediğimiz satırdan sonraki işlemleri izleyebiliriz
+           *   Watch penceresi karmaşık yapıların sonuçlarını gösterir 
+           *
+           */
+            Console.WriteLine("");
+        }
+    }
+}
